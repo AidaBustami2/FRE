7d3313c252a5f952312ebb0e70a71184c7f3abeb
AidaBustami2 <47245468+AidaBustami2@users.noreply.github.com>
2022-03-06 03:19:31 -0500

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-if ((($(docker images |grep img1 |wc -l) > 0 )) && (($(docker ps -a  |grep img1 |wc -l) > 0)))
+
+if (($(docker ps -a  |grep AIDA2 |wc -l) > 0 ))
+then
+ docker stop $(docker ps -a | grep AIDA2 | awk '{print $1}')
+ docker rm $(docker ps -a | grep AIDA2 | awk '{print $1}')
+fi
+if ((($(docker images |grep img1 |wc -l) > 0 )) && (($(docker ps -a  |grep img1 |wc -l) > 0)) && (($(docker ps -a  |grep AIDA2 |wc -l) > 0)))
 then
  docker stop $(docker ps -a | grep img1 | awk '{print $1}')
  docker rm $(docker ps -a | grep img1 | awk '{print $1}')
+ docker rm $(docker ps -a | grep AIDA2 | awk '{print $1}')
  docker rmi img1
- echo "not zero"
-else
- echo "zero"
 fi
-
-
-
